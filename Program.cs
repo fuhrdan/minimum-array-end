fea2834ad9db357c4258b09a3fdd52ce2ffcde80
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-09 09:15:53 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//*****************************************************************************
+//** 3133. Minimum Array End    leetcode                                     **
+//*****************************************************************************
+
+
+long long minEnd(int n, int x) {
+    n--;
+    long long retNum = x;
+    for (int i = 0; i < 31; i++) {
+        if (((x >> i) & 1) ^ 1) {
+            retNum |= (long long)((n & 1) << i);
+            n >>= 1;
+        }
+    }
+    retNum |= ((long long)n) << 31;
+    return retNum;
+}
